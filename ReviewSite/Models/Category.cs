9096df976ac1909b62e0e87eee1b6a0282b9b975
Your Name <you@example.com>
2017-07-28 11:27:29 -0400

    added models, controllers, navbar styling

---
+++ ReviewSite/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ ReviewSite/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewContent { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string ReviewAuthor { get; set; }
+        public decimal ReviewScore { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
